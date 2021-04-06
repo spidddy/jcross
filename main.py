fb27ad30c029ca72c6a68c86ab61ab0f61d373c4
spidddy <spider@nofate.ru>
2021-04-06 12:36:48 +0400

    Дорисовал интерфейс

--- main.py
+++ main.py
@@ -6,16 +6,22 @@
 
 import tkinter as tk
 
+
 # Временные переменные для отрисовки интерфейса
 cr_width = 10
 cr_height = 10
 
-columns = [[1, 2], [3, 2], [3, 1], [1, 4], [1], [3, 1], [3, 1, 3, 1, 1], [5, 3], [4, 3], [4, 1]]
-rows = [[2, 4], [4, 4], [2, 5], [1, 3], [5], [1], [1, 3], [3, 5], [2, 3, 1, 1, 1], [1]]
+# columns = [[1, 2], [3, 2], [3, 1], [1, 4], [1], [3, 1], [3, 1, 3], [5, 3], [4, 3], [4, 1]]
+# rows = [[2, 4], [4, 4], [2, 5], [1, 3], [5], [1], [1, 3], [3, 5], [2, 3], [1]]
+
+#################################################
 
 root = tk.Tk()
 root.title('Решаем японские кроссворды')
 # root.geometry('800x600')
+columns = []
+rows = []
+
 c_max = 0
 for i in columns:
     if len(i) > c_max:
@@ -24,21 +30,20 @@ r_max = 0
 for i in rows:
     if len(i) > r_max:
         r_max = len(i)
-
 head = tk.Frame(root)
 numbers = tk.Entry(head, width=30).grid(row=0, column=0)
-tk.Button(head, text='Ввод').grid(row=0, column=1)
-tk.Button(head, text='Очистить').grid(row=0, column=2)
+tk.Button(head, text='Доб. строку').grid(row=0, column=1)
+tk.Button(head, text='Доб. столбец').grid(row=0, column=2)
+tk.Button(head, text='Очистить').grid(row=0, column=3)
 head.pack(fill=tk.Y)
 
 body = tk.Frame(root)
 
 top_left = tk.Frame(body)
+for i in range(c_max):
+    for j in range (r_max):
+        tk.Frame(top_left, width=25, height=25, bg='green').grid(row=i, column=j)
 
-tk.Frame(top_left, width=25, height=25, bg='green').grid(row=0, column=0)
-tk.Button(top_left, text='+').grid(row=0, column=1, sticky='nesw')
-tk.Button(top_left, text='+').grid(row=1, column=0, sticky='nesw')
-tk.Frame(top_left, width=25, height=25, bg='green').grid(row=1, column=1)
 top_left.pack(side=tk.LEFT)
 
 top_right = tk.Frame(body)
@@ -48,8 +53,8 @@ for i in range(len(columns)):
             txt = ''
         else:
             txt = str(columns[i][j - (c_max - len(columns[i]))])
-        sq = tk.Frame(top_right, width=25, height=25, borderwidth=1, relief=tk.RIDGE)
-        tk.Label(sq, text=txt).grid(row=0, column=0)
+        sq = tk.Frame(top_right, width=25, height=25, bg='light gray', borderwidth=1, relief=tk.RIDGE)
+        tk.Label(sq, text=txt, bg='light gray').grid(row=0, column=0)
         sq.grid(row=j, column=i, sticky='snwe')
     top_right.columnconfigure(i, minsize=25)
 top_right.pack(side=tk.LEFT)
@@ -63,8 +68,8 @@ for i in range(len(rows)):
             txt = ''
         else:
             txt = str(rows[i][j - (r_max - len(rows[i]))])
-        sq = tk.Frame(left, width=25, height=25, borderwidth=1, relief=tk.RIDGE)
-        tk.Label(sq, text=txt).grid(row=0, column=0)
+        sq = tk.Frame(left, width=25, height=25, bg='light gray', borderwidth=1, relief=tk.RIDGE)
+        tk.Label(sq, text=txt, bg='light gray').grid(row=0, column=0)
         sq.grid(row=i, column=j, sticky='snwe')
     left.columnconfigure(i, minsize=25)
 left.pack(side=tk.LEFT)
