dd07882a2cbb860f6ea55e109ba328c868e65b70
spidddy <spider@nofate.ru>
2021-04-08 11:44:55 +0400

    вроде рабоет

--- jcross.py
+++ jcross.py
@@ -2,27 +2,46 @@ import tkinter as tk
 
 
 def add_row():
-    global rows, canv_h
+    global rows, canv_h, canv_w
     num = numbers.get().split()
+    if len(num) == 0:
+        return
     for i in range(len(num)):
         num[i] = int(num[i])
     rows.append(num)
     numbers.delete(0, tk.END)
-    return
-    left_side(10, 10 + col_max() * 24)
     canv_height = (len(rows) + col_max()) * 24 + 20
     if canv_h < canv_height:
         canv_h = canv_height
         canv.config(height=canv_h)
-        canv.update()
+    canv_width = (len(columns) + row_max()) * 24 + 20
+    if canv_w < canv_width:
+        canv_w = canv_width
+        canv.config(height=canv_w)
+    paint()
 
 def add_col():
-    pass
+    global columns, canv_w, canv_h
+    num = numbers.get().split()
+    if len(num) == 0:
+        return
+    for i in range(len(num)):
+        num[i] = int(num[i])
+    columns.append(num)
+    numbers.delete(0, tk.END)
+    canv_width = (len(columns) + row_max()) * 24 + 20
+    if canv_w < canv_width:
+        canv_w = canv_width
+        canv.config(height=canv_w)
+    canv_height = (len(rows) + col_max()) * 24 + 20
+    if canv_h < canv_height:
+        canv_h = canv_height
+        canv.config(height=canv_h)
+    paint()
+
 
 def paint():
-    canv = tk.Canvas(root, width=canv_w, height=canv_h)
-    canv.pack()
-
+    canv.create_rectangle(10, 10, 10 + row_max() * 24, 10 + col_max() * 24, fill='gray')
     left_side(10, 10 + col_max() * 24)
     top_side(10 + row_max() * 24, 10)
 
@@ -32,20 +51,21 @@ def left_side(x, y):
         for j in range(row_max()):
             a = x + j * 24
             b = y + i * 24
-            canv.create_rectangle(a, b, a+24, b+24)
+            canv.create_rectangle(a, b, a + 24, b + 24, fill='light gray')
             if j >= row_max() - len(rows[i]):
                 amendment = row_max() - len(rows[i])
-                canv.create_text(a+12, b+12, text=rows[i][j-amendment])
+                canv.create_text(a + 12, b + 12, text=rows[i][j - amendment])
+
 
 def top_side(x, y):
     for i in range(len(columns)):
         for j in range(col_max()):
             a = x + i * 24
             b = y + j * 24
-            canv.create_rectangle(a, b, a+24, b+24)
+            canv.create_rectangle(a, b, a + 24, b + 24, fill='light gray')
             if j >= col_max() - len(columns[i]):
                 amendment = col_max() - len(columns[i])
-                canv.create_text(a+12, b+12, text=columns[i][j-amendment])
+                canv.create_text(a + 12, b + 12, text=columns[i][j - amendment])
 
 
 def row_max():
@@ -65,11 +85,12 @@ def col_max():
             max_value = len(col)
     return max_value
 
+
 canv_w = 500
 canv_h = 300
 rows = []
 columns = []
-columns = [[1, 2], [3, 2], [3, 1], [1, 4], [1], [3, 1], [3, 1, 3], [5, 3], [4, 3], [4, 1]]
+# columns = [[1, 2], [3, 2], [3, 1], [1, 4], [1], [3, 1], [3, 1, 3], [5, 3], [4, 3], [4, 1]]
 rows = [[2, 4], [4, 4], [2, 5], [1, 3], [5], [1], [1, 3], [3, 5], [2, 3], [1]]
 
 root = tk.Tk()
@@ -82,11 +103,8 @@ tk.Button(head, text='Решить').grid(row=0, column=3)
 tk.Button(head, text='Очистить').grid(row=0, column=4)
 numbers.grid(row=0, column=0)
 head.pack(fill=tk.Y)
+
 canv = tk.Canvas(root, width=canv_w, height=canv_h)
 canv.pack()
-
-left_side(10, 10 + col_max() * 24)
-top_side(10 + row_max() * 24, 10)
-
-
+paint()
 root.mainloop()
